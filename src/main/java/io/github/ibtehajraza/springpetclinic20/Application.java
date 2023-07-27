28f814cb1f1cb3c006ec3909f8524bccf0855a99
ibtehajraza <lassena@lassena.etsmtl.ca>
2023-07-26 20:59:50 -0400

    Project added.

---
+++ src/main/java/io/github/ibtehajraza/springpetclinic20/Application.java
@@ -0,0 +1,13 @@
+package io.github.ibtehajraza.springpetclinic20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ibtehajraza/springpetclinic20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ibtehajraza.springpetclinic20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
