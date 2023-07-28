fd08f8354611c73fd194dddc18772092c8799cd0
ibtehajraza <ibtahajraza@gmail.com>
2023-07-27 21:25:20 -0400

    Implemented Owners Index page and controller: Added root RequestMapping for OwnerController

--- pet-clinic-web/src/main/java/io/github/ibtehajraza/springpetclinic20/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/io/github/ibtehajraza/springpetclinic20/controller/OwnerController.java
@@ -3,9 +3,10 @@ package io.github.ibtehajraza.springpetclinic20.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"/owners", "/owners/list", "owners/index", "owners/index.html"})
+    @RequestMapping({"", "/", "/list", "/index", "/index.html"})
     public String listOwners() {
         return "owners/index";
     }
