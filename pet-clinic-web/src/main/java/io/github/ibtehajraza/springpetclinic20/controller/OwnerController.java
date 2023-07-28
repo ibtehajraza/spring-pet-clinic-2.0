fc1df3e33a2ae0eea1c2473681e1539fa12c9002
ibtehajraza <ibtahajraza@gmail.com>
2023-07-27 21:20:40 -0400

    Implemented Owners Index page and controller, closes #5

--- pet-clinic-web/src/main/java/io/github/ibtehajraza/springpetclinic20/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/io/github/ibtehajraza/springpetclinic20/controller/OwnerController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetController {
-
-    @RequestMapping({"/vets", "/vets/list", "vets/index", "vets/index.html"})
-    public String listVets() {
-        return "vets/index";
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/list", "owners/index", "owners/index.html"})
+    public String listOwners() {
+        return "owners/index";
     }
 }
